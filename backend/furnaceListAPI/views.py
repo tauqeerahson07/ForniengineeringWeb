cf644430914294b921aac721522d841d5abbe6a5
tauqeerahson07 <97336093+tauqeerahson07@users.noreply.github.com>
2024-08-17 12:07:19 +0500

    intial commit

---
+++ backend/furnaceListAPI/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+# Create your views here.
+@api_view()
+def furnaces(request):
+    return Response('list of furnaces',status=status.HTTP_200_OK)
