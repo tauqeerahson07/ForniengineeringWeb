b45b1e01083872ae92902dd1645f355d4a0d05c1
tauqeerahson07 <97336093+tauqeerahson07@users.noreply.github.com>
2025-01-19 20:09:07 +0500

    feat: initialize Next.js project with Tailwind CSS, ESLint, and basic API setup

--- backend/DataRetrivalAPI/urls.py
+++ backend/DataRetrivalAPI/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('furnaces/',views.furnaces)
+    path('furnaces/',views.furnaces),
+    path('furnaces/<str:name>/',views.SearchFindFurnace, name = 'Search'),
 ]
--- backend/DataRetrivalAPI/views.py
+++ backend/DataRetrivalAPI/views.py
@@ -3,9 +3,29 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 from .models import Furnaces
-
+from .serializer import FurnacesSerializer
+from rest_framework import status
 # Create your views here.
+
 @api_view(['GET'])
 def furnaces(request):
     furnaces = Furnaces.objects.all()
-    return Response(furnaces)
+    serializer = FurnacesSerializer(furnaces, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def SearchFindFurnace(request, name):
+    # Filter furnaces by the name (URL parameter)
+    furnaces = Furnaces.objects.filter(name__icontains=name).all()
+    print(furnaces.query)
+    if furnaces.exists():
+        # Serialize the filtered queryset
+        serializer = FurnacesSerializer(furnaces, many=True)
+        return Response(serializer.data)
+    else:
+        # Return a 404 response if no furnaces are found
+        return Response(
+            {"error": f"No furnaces found containing the name '{name}'."},
+            status=status.HTTP_404_NOT_FOUND
+        )
