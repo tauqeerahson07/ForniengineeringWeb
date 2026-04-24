74b5a451d3e3c57c41a6bc5bfce49f1f3bae1036
Tauqeer Ahson <97336093+tauqeerahson07@users.noreply.github.com>
2026-04-24 22:59:48 +0500

    Update MEDIA_URL to use a static path instead of R2_S3_ENDPOINT_URL

--- backend/forni_backend/settings.py
+++ backend/forni_backend/settings.py
@@ -17,8 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'frontend', 'forni-web', 'public','webpage')
 # MEDIA_URL = '/webpage/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL = '/media/'
-MEDIA_URL = os.path.join(os.getenv("R2_S3_ENDPOINT_URL"),'/')
+MEDIA_URL = '/media/'
 
 # Point to the root directory .env file
 ROOT_DIR = BASE_DIR.parent  # This goes up one more level to the project root
