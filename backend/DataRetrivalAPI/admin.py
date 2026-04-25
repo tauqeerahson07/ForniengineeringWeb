8914a5ca6e1908bd5f814377a44028052a2c1656
Tauqeer Ahson <97336093+tauqeerahson07@users.noreply.github.com>
2026-04-25 12:57:23 +0500

    Refactor R2 storage integration: switch from Supabase to boto3 for file handling and update requirements

--- backend/DataRetrivalAPI/admin.py
+++ backend/DataRetrivalAPI/admin.py
@@ -4,13 +4,32 @@ from django.utils.text import slugify
 import os
 from supabase import create_client
 from dotenv import load_dotenv
+import boto3
+from botocore.config import Config
 load_dotenv()
+
+# Your Cloudflare credentials
+account_id = os.getenv("R2_ACCOUNT_ID")
+access_key = os.getenv("R2_ACCESS_KEY_ID")
+secret_key = os.getenv("R2_SECRET_ACCESS_KEY")
+bucket_name = os.getenv("BUCKET")
+
+# Configure the S3 client for R2
+s3 = boto3.client(
+    service_name="s3",
+    endpoint_url=f"https://{account_id}.r2.cloudflarestorage.com",
+    aws_access_key_id=access_key,
+    aws_secret_access_key=secret_key,
+    region_name="auto",  # R2 uses "auto" or "us-east-1"
+    config=Config(signature_version="s3v4"),
+)
+
 
 
 bucket_name = os.getenv("BUCKET")
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SERVICE_ROLE_KEY")
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+# SUPABASE_URL = os.getenv("SUPABASE_URL")
+# SUPABASE_KEY = os.getenv("SERVICE_ROLE_KEY")
+# supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # Helper function to delete folder from Supabase Storage
 def delete_supabase_folder(bucket, prefix):
@@ -19,12 +38,12 @@ def delete_supabase_folder(bucket, prefix):
 
         while stack:
             current = stack.pop()
-            items = supabase.storage.from_(bucket).list(current)
+            items = s3.list_objects_v2(Bucket=bucket, Prefix=current)
 
             files_to_delete = []
 
-            for item in items:
-                name = item["name"]
+            for item in items.get("Contents", []):
+                name = item["Key"]
                 full_path = f"{current}/{name}"
 
                 if item.get("metadata"):  # file
@@ -33,7 +52,7 @@ def delete_supabase_folder(bucket, prefix):
                     stack.append(full_path)
 
             if files_to_delete:
-                supabase.storage.from_(bucket).remove(files_to_delete)
+                s3.delete_objects(Bucket=bucket, Delete={"Objects": [{"Key": key} for key in files_to_delete]})
 
     except Exception as e:
         print("Supabase recursive delete error:", e)
